ecd89baea72cb993fb8360faa4a7dc92c571c0ad
protocols <protocols@example.com>
2023-10-10 14:47:24 +0900

    xxx

--- src/main/java/com/example/controller/AjaxController.java
+++ src/main/java/com/example/controller/AjaxController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.util.Alpha;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
 
 @Controller
 public class AjaxController {
